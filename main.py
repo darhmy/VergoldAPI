9b777dd99365a7ac90ad1a726ba31ff3ce62f9ea
PetrOgundipe <ogundipepeterdare@gmail.com>
2024-11-07 13:42:51 +0100

    Adjust main file

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ app.include_router(auth_api.router, prefix="/api/v1/auth", tags=["Authetication"
 
 app.include_router(profile_api.router, prefix="/api/v1/profile", tags=["Profile"])
 
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Vergold Web API Documentation"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+
